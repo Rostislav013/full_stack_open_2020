ff969a4a2f453a6d8faeddf3319749ffec4f6f30
Rostislav013 <rostislav013@gmail.com>
2020-03-26 13:06:44 +0200

    make a base

---
+++ part2/courseinfo/README.md
@@ -0,0 +1,2 @@
+# Part 1
+- Course info task
---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+    </div>
+  )
+};
+
+const Part = (props) => {
+
+  return(
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return(
+    <div>
+       { props.course.parts.map((el, i) => {
+          return <Part key={i} name={el.name} exercises={el.exercises} />
+       })}
+    </div>
+  );
+};
+
+const Total = (props) => {
+  let sum = 0;
+  return(
+    <div>
+      {props.course.parts.map((el, i) => {
+         sum += el.exercises
+      })}
+      <p>Number of exercises {sum} </p>
+    </div>
+  );
+}
+
+const App = () => {
+  const course = {
+   name: 'Half Stack application development',
+   parts: [
+     {
+       name: 'Fundamentals of React',
+       exercises: 10
+     },
+     {
+       name: 'Using props to pass data',
+       exercises: 7
+     },
+     {
+       name: 'State of a component',
+       exercises: 14
+     }
+   ]
+ }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course}  />
+      <Total course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
