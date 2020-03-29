95d9b76e00d46b94091857acf030e8773809fb1d
Rostislav013 <rostislav013@gmail.com>
2020-03-29 13:04:24 +0300

    task 2.8 step 3 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number: '0442399527'}
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
+  const handleNewName = (event) => {
+      console.log(event.target.value)
+      setNewName(event.target.value)
+  }
+  const handleNewNumber = (event) => {
+      console.log(event.target.value)
+      setNewNumber(event.target.value)
+  }
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    const names = persons.map(el => el.name) // array of existid names
+
+    if(newName === '' || newNumber === '') {
+      alert("Type a name with number")
+    } else if(names.includes(newName)) {
+      alert(`${newName} is already in phonebook.`)
+    }  else  {
+      setPersons(persons.concat(personObject))
+      setNewName('') // reset input
+      setNewNumber('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form id="nameForm" onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+          <div>number: <input type="number" value={newNumber} onChange={handleNewNumber} placeholder="ex. 0441234567"/></div>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, i) =>
+          <li key={person.name} style={{listStyle: "none"}}>{person.name} {person.number}</li>
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
