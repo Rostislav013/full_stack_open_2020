06ced686d32753abae2749dacc773201b1de9047
Rostislav013 <rostislav013@gmail.com>
2020-07-04 10:12:12 +0300

    3.7 phonebook backend step7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 /*In order to access the data easily, 
 we need the help of the express json-parser, 
 that is taken to use with command app.use(express.json()). */
 app.use(express.json());
 
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
+
+console.log(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+    ].join(" ");
+  })
+);
 let persons = [
   {
     name: "Arto Hellas",
